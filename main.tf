2071551279e392ca649f11fd852aafd66db3e062
Your Name <you@example.com>
2026-03-10 12:43:51 +0530

    rahul xyz

---
+++ main.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "name" {
+  name = "asnism"
+  location = "eastusy"
+}
---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.63.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = "c9c6876b-182e-430d-87e0-5fbf18c2f9cf"
+  features {
+    
+  }
+}
